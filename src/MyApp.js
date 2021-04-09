8ef37e33055c668bc4a1d6d99593c2a2ccd0763b
David <dreko@calpoly.edu>
2021-04-09 08:57:55 -0700

    return json

--- src/MyApp.js
+++ src/MyApp.js
@@ -42,7 +42,7 @@ function MyApp() {
   async function makePostCall(person){
     try {
        const response = await axios.post('http://localhost:5000/users', person);
-       return response;
+       return response.data;
     }
     catch (error) {
        console.log(error);
